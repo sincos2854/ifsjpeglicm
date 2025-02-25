fd315a0a9a26391c6a13256b39b6682d2b765bf9
sincos2854 <sincos2854dev@gmail.com>
2025-02-25 13:35:06 +0900

    Update jpegli to google/jpegli@bc19ca2

--- build_checkout_jpegli.cmd
+++ build_checkout_jpegli.cmd
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 if not exist "jpegli" (
     git clone https://github.com/google/jpegli.git --recursive
     cd jpegli
-    git checkout 8d333ebb0d715b0544a59d29724af9f8155aeb52
+    git checkout bc19ca2393f79bfe0a4a9518f77e4ad33ce1ab7a
     cd third_party\libjpeg-turbo
     git fetch origin main
     git checkout refs/tags/2.1.91
