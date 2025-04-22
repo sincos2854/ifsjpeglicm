538f990d88e941204cf4218b3c838c18d890ec12
sincos2854 <sincos2854dev@gmail.com>
2025-04-22 11:32:47 +0900

    Use the correct commit of libjpeg-turbo and apply the patch

--- build_checkout_jpegli.cmd
+++ build_checkout_jpegli.cmd
@@ -3,10 +3,5 @@ cd /d "%~dp0"
 
 if not exist "jpegli" (
     git clone https://github.com/google/jpegli.git --recursive
-    cd jpegli
-    git checkout bc19ca2393f79bfe0a4a9518f77e4ad33ce1ab7a
-    cd third_party\libjpeg-turbo
-    git fetch origin main
-    git checkout refs/tags/2.1.91
-    cd ..\..\..\
+    git apply -p1 --directory=jpegli/third_party/libjpeg-turbo patch_libjpeg_turbo.patch
 )
