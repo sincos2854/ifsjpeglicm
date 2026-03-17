5008412d66488cf0062e3a52fa0c4514b57849db
sincos2854 <sincos2854dev@gmail.com>
2026-03-17 18:15:43 +0900

    Add --recurse-submodules to git checkout

--- build_checkout_jpegli.cmd
+++ build_checkout_jpegli.cmd
@@ -5,7 +5,7 @@ if not exist "jpegli" (
     git clone https://github.com/google/jpegli.git --recursive
 
     cd jpegli
-    git checkout bc19ca2393f79bfe0a4a9518f77e4ad33ce1ab7a
+    git checkout --recurse-submodules bc19ca2393f79bfe0a4a9518f77e4ad33ce1ab7a
     cd ..
 
     git apply -p1 --directory=jpegli/third_party/libjpeg-turbo patch_libjpeg_turbo.patch
