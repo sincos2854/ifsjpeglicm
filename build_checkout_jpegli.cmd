a207030d0f2b295035dcba4baa7a824bd0225d4b
sincos2854 <sincos2854dev@gmail.com>
2026-03-20 07:35:23 +0900

    Update jpegli to google/jpegli@12ac27c

--- build_checkout_jpegli.cmd
+++ build_checkout_jpegli.cmd
@@ -5,7 +5,7 @@ if not exist "jpegli" (
     git clone https://github.com/google/jpegli.git --recursive
 
     cd jpegli
-    git checkout --recurse-submodules bc19ca2393f79bfe0a4a9518f77e4ad33ce1ab7a
+    git checkout --recurse-submodules 12ac27c46927f94eab7e8eaf1e14e6419ab55d33
     cd ..
 
     git apply -p1 --directory=jpegli/third_party/libjpeg-turbo patch_libjpeg_turbo.patch
