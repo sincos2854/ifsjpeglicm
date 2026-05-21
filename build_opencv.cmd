59dfd2d71c20c62f393266d2257ffcf2209de8b1
sincos2854 <sincos2854dev@gmail.com>
2026-05-21 13:47:36 +0900

    Optimize OpenCV build options

--- build_opencv.cmd
+++ build_opencv.cmd
@@ -35,58 +35,59 @@ exit /b
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>" ^
     -DBUILD_SHARED_LIBS=OFF ^
-    -DBUILD_TESTS=OFF ^
-    -DBUILD_PERF_TESTS=OFF ^
-    -DBUILD_EXAMPLES=OFF ^
-    -DBUILD_opencv_apps=OFF ^
-    -DWITH_OPENCL=OFF ^
+    -DBUILD_LIST=core ^
+    -DBUILD_opencv_imgcodecs=OFF ^
+    -DBUILD_opencv_highgui=OFF ^
+    -DBUILD_IPP_IW=OFF ^
+    -DWITH_ADE=OFF ^
+    -DWITH_1394=OFF ^
+    -DWITH_AVIF=OFF ^
+    -DWITH_VTK=OFF ^
+    -DWITH_EIGEN=OFF ^
     -DWITH_FFMPEG=OFF ^
     -DWITH_GSTREAMER=OFF ^
-    -DWITH_MSMF=OFF ^
-    -DWITH_DSHOW=OFF ^
-    -DWITH_1394=OFF ^
-    -DWITH_PROTOBUF=OFF ^
-    -DVIDEOIO_ENABLE_PLUGINS=OFF ^
-    -DOPENCV_DNN_OPENCL=OFF ^
-    -DBUILD_opencv_dnn=OFF ^
-    -DBUILD_opencv_features2d=OFF ^
-    -DBUILD_opencv_flann=OFF ^
-    -DBUILD_opencv_gapi=OFF ^
-    -DBUILD_opencv_highgui=OFF ^
-    -DBUILD_opencv_imgcodecs=OFF ^
-    -DBUILD_opencv_imgproc=OFF ^
-    -DBUILD_opencv_ml=OFF ^
-    -DBUILD_opencv_photo=OFF ^
-    -DBUILD_opencv_video=OFF ^
-    -DBUILD_opencv_videoio=OFF ^
-    -DWITH_TBB=OFF ^
-    -DBUILD_TBB=OFF ^
     -DWITH_IPP=OFF ^
-    -DBUILD_IPP_IW=OFF ^
-    -DBUILD_ITT=OFF ^
+    -DWITH_JASPER=OFF ^
+    -DWITH_OPENJPEG=OFF ^
+    -DWITH_JPEG=OFF ^
+    -DWITH_WEBP=OFF ^
+    -DWITH_OPENEXR=OFF ^
+    -DWITH_PNG=OFF ^
     -DWITH_WIN32UI=OFF ^
+    -DWITH_TBB=OFF ^
+    -DWITH_TIFF=OFF ^
+    -DWITH_DSHOW=OFF ^
+    -DWITH_MSMF=OFF ^
+    -DWITH_OPENCL=OFF ^
     -DWITH_DIRECTX=OFF ^
+    -DWITH_DIRECTML=OFF ^
+    -DWITH_LAPACK=OFF ^
+    -DWITH_PROTOBUF=OFF ^
+    -DWITH_IMGCODEC_GIF=OFF ^
     -DWITH_IMGCODEC_HDR=OFF ^
     -DWITH_IMGCODEC_SUNRASTER=OFF ^
     -DWITH_IMGCODEC_PXM=OFF ^
     -DWITH_IMGCODEC_PFM=OFF ^
-    -DBUILD_ZLIB=OFF ^
-    -DWITH_PNG=OFF ^
-    -DWITH_JPEG=OFF ^
-    -DWITH_TIFF=OFF ^
-    -DWITH_WEBP=OFF ^
-    -DWITH_OPENJPEG=OFF ^
-    -DWITH_JASPER=OFF ^
-    -DWITH_OPENEXR=OFF
+    -DWITH_OBSENSOR=OFF ^
+    -DBUILD_opencv_apps=OFF ^
+    -DBUILD_DOCS=OFF ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_PACKAGE=OFF ^
+    -DBUILD_PERF_TESTS=OFF ^
+    -DBUILD_TESTS=OFF ^
+    -DBUILD_JAVA=OFF ^
+    -DOPENCV_PYTHON_SKIP_DETECTION=ON ^
+    -DCV_TRACE=OFF ^
+    -DBUILD_opencv_world=OFF
 
     cmake --build "build_%BUILD_ARCH%_release" --config Release --target install
     
     cd ..\
 
 :copy_files
-    xcopy /s /y "opencv\out_%BUILD_ARCH%_release\include\opencv2\*" "include\%BUILD_ARCH%\opencv2\"
+    xcopy "opencv\out_%BUILD_ARCH%_release\include\opencv2\*" "include\%BUILD_ARCH%\opencv2\"  /y /s /q
 
-    for /f "usebackq delims=" %%a in (`where /r "opencv\out_%BUILD_ARCH%_release" opencv*.lib`) do set "LIB_PATH=%%a"
-    xcopy /y "%LIB_PATH%" "lib\%BUILD_ARCH%\Release\"
+    for /f "usebackq delims=" %%i in (`where /r "opencv\out_%BUILD_ARCH%_release" opencv*.lib`) do set "LIB_PATH=%%i"
+    xcopy "%LIB_PATH%" "lib\%BUILD_ARCH%\Release\" /y
 
     exit /b
